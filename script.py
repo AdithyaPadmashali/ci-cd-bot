6085cefb0ac3d0bb40b29e162832646b334d7a6c
AdithyaPadmashali <adithya.p2001@gmail.com>
2021-07-11 21:57:55 +0530

    removed unwanted comments

--- script.py
+++ script.py
@@ -10,10 +10,6 @@ dotenv.load_dotenv('./.env')
 
 option = sys.argv[1]
 
-# owner = 'AdithyaPadmashali'
-
-# repository_name = "ci-cd-pipeline-using-github-actions"
-
 #Get the latest workflow status
 if(option == '--get' or option =='-g'):
     latest_workflow_status = requests.get('https://api.github.com/repos/{owner}/{repository_name}/actions/runs?per_page=1'.format(owner = os.environ['user'], repository_name = os.environ['repo']))
@@ -27,11 +23,9 @@ elif(option == '--post' or option == '-p'):
     #Trigger a workflow
     key = os.getenv('repo_access_auth_key')
     auth_headers = {'Authorization' : 'Bearer {key}'.format(key = key)}
-    # print(auth_headers)
     payload = {"ref" : "heroku"}
     print(os.environ['user'])
     triggered = requests.post('https://api.github.com/repos/{owner}/{repository_name}/actions/workflows/main-workflow.yml/dispatches'.format(owner = os.environ['user'], repository_name = os.environ['repo']), data = json.dumps(payload), headers = auth_headers) 
-    # print(triggered.content)
 
 elif(option == '--set' or option == '-s'): 
     if([sys.argv[2], sys.argv[4]] != ['--user', '--repo']):
@@ -43,8 +37,6 @@ elif(option == '--set' or option == '-s'):
         dotenv.set_key(dotenv_file, "user", os.environ["user"])
         dotenv.set_key(dotenv_file, "repo", os.environ["repo"])
     
-
-
 else:
     print("Enter the right argument")
 
